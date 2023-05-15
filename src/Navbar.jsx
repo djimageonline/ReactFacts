aff6596e456f46694a8457859c1d849ceb91b226
Johnny Proano <jproano22@yahoo.com>
2023-05-15 11:16:27 -0400

    added initial and 3 componenets for now

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import reactLogo from "./assets/react.svg";
+
+export default function Navbar() {
+  return (
+    <div className="header-container">
+      <div className="left-div">
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <p> ReactFacts</p>
+      </div>
+      <nav>NavBar goes here</nav>
+    </div>
+  );
+}
